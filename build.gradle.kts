e94d88d3bba0e7a4fd6ff31b3873ffc3ea406bf8
wangshaopeng <1061909924@qq.com>
2025-03-09 01:18:09 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij.platform") version "2.2.0"
+    id("org.jetbrains.intellij.platform") version "2.3.0"
 }
 
 group = "com.testkit"
